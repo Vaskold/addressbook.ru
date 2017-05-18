3efffdcb9f3ac723b33b3b430fb06aa6a48177a1
Vaskold <s0103465@edu.kubsu.ru>
2017-05-18 19:55:00 +0300

    address book

---
+++ www/index.php
@@ -0,0 +1,22 @@
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+
+This is normal HTML code
+
+<?php
+    $var = 5;
+?>
+
+$var = 10;<br />
+
+The variable $var has a value of:
+
+<?=$var?><br />
+
+Back into normal HTML
+
+</body>
+</html>
